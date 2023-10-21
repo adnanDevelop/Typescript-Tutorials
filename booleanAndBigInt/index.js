0181e8a3d08d168616454b76cdc5cab60dc28481
Adnan Tariq <adnan6official@gmail.com>
2023-10-21 12:14:42 +0500

    In this lecture i learn bigint and boolean type in next video i will learn unknown and any type

---
+++ booleanAndBigInt/index.js
@@ -0,0 +1,26 @@
+"use strict";
+// BOOLEAN AND BIGINT TYPES
+let booleanType = true; // simple add boolean at the end of variable name
+// write a function that take a number as a parameter and return true if the number is even and false otherwise;
+// const isEven = (value:number):boolean => {
+//     if(value % 2 === 0) {
+//         return true;
+//     } else {
+//         return false
+//     }
+// };
+// console.log(isEven(10));
+// Write a function that take number that divided by 4 or 8 return true if divided by these integers otherwise false
+// const isDivided = (value: number): boolean => {
+//   if (value % 4 === 0 && value % 8 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+// console.log(isDivided(40));
+// BIGINT DATA TYPE;
+const maxNumber = 9007199254740991n;
+const newNumber = 9007199254740991n;
+const addbigInit = maxNumber + newNumber;
+console.log(addbigInit);
